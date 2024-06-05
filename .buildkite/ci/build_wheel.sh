dd4795c0b153c7ba5e1c2c8191c363b37a0a2731
sang <rkooo567@gmail.com>
2024-06-05 01:24:29 -0700

    D.

--- .buildkite/ci/build_wheel.sh
+++ .buildkite/ci/build_wheel.sh
@@ -11,9 +11,23 @@ VLLM_WHEEL="vllm-0.1.4-cp39-cp39-linux_x86_64.whl"
 rm -rf dist
 rm -rf build
 
-# Download cmake
-sudo apt-get update
-sudo apt-get install -y cmake
+# Download the latest cmake. vLLM requires cmake 3.22. It is easy to download in ubuntu 22.04, but we are using ubuntu 20.04.
+# https://askubuntu.com/questions/355565/how-do-i-install-the-latest-version-of-cmake-from-the-command-line
+# sudo apt-get update
+# sudo apt-get install -y cmake
+sudo apt update && \
+sudo apt install -y software-properties-common lsb-release && \
+sudo apt clean all
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+sudo apt update
+sudo apt install kitware-archive-keyring
+sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF7F09730B3F0A4
+sudo apt update
+sudo apt install -y cmake
+
+
 
 echo "~~~ :python: Building wheel for ${VLLM_PROJECT}@${GIT_COMMIT}"
 BUILD_BAZEL=1 python setup.py bdist_wheel
