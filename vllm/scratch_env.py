601cd60ef69d89fb7ad23ddef4b9a445257aa259
sang <rkooo567@gmail.com>
2024-06-28 10:29:55 -0700

    working

--- vllm/scratch_env.py
+++ vllm/scratch_env.py
@@ -8,7 +8,9 @@ SCRATCH_EXECUTABLE_PATH_ENV_VAR = "SCRATCH_EXECUTABLE_PATH"
 # SANG-TODO A10
 SCRATCH_BUILD_PREFIX = "ll38b-s1-cuda-f16" # CHANGE THIS FOR DIFFERNT MODELS
 SCRATCH_BUILD_TYPE = "fullopt" # We should remove this, this is needed because weights are the same for all builds types.
-SCRATCH_EXECUTABLE_PATH =os.getenv(SCRATCH_EXECUTABLE_PATH_ENV_VAR, f"./vllm/scratch-{SCRATCH_BUILD_PREFIX}-{SCRATCH_BUILD_TYPE}.cpython-39-x86_64-linux-gnu.so")
+# Hack.
+current_directory = os.path.dirname(os.path.abspath(__file__))
+SCRATCH_EXECUTABLE_PATH =os.getenv(SCRATCH_EXECUTABLE_PATH_ENV_VAR, f"{current_directory}/scratch-{SCRATCH_BUILD_PREFIX}-{SCRATCH_BUILD_TYPE}.cpython-39-x86_64-linux-gnu.so")
 SCRATCH_WEIGHTS_BUCKET_NAME = "scratch-working-dirs"
 SCRATCH_WEIGHTS_PREFIX = f"staging_weights/{SCRATCH_BUILD_PREFIX}/"
 SCRATCH_WEIGHTS_URI = f"s3://{SCRATCH_WEIGHTS_BUCKET_NAME}/{SCRATCH_WEIGHTS_PREFIX}"
