3831f47b545b1aebfbc5f2ec59ab76a8249b9412
sang <rkooo567@gmail.com>
2024-07-03 06:44:07 -0700

    lint

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -10,7 +10,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-
 try:
     from flashinfer import BatchDecodeWithPagedKVCacheWrapper
     from flashinfer.decode import CUDAGraphBatchDecodeWithPagedKVCacheWrapper
